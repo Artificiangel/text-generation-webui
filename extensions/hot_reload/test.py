e7d81ec01a45ad82c6d1c1161d17ed89ccfba9d0
Artificiangel <cca@aquilatech.com>
2024-05-01 05:52:32 -0400

    Convert test script from ipynb to py

--- extensions/hot_reload/test.py
+++ extensions/hot_reload/test.py
@@ -2,7 +2,12 @@ from au import setup
 setup(debug=True)
 from au.ext.ai.model_text_gen import TextGen
 
-async with TextGen() as tg:
-    res = await tg.complete('Test', max_tokens=10)
-    print(res.text)
-    print(len(res.choices))
+async def main():
+    async with TextGen() as tg:
+        res = await tg.complete('Test', max_tokens=10)
+        print(res.text)
+        print(len(res.choices))
+
+import asyncio
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
